9c94529fe56ae702ae82b5b951fe4871e8b69e14
Naidim214 <156600024+Naidim214@users.noreply.github.com>
2024-01-14 22:06:21 +0530

    Add files via upload
    this one is testing purpose

---
+++ 6.pandasanalyzingdataframe.py
@@ -0,0 +1,12 @@
+import pandas as pd
+nadim=pd.read_csv('data.csv')
+print(nadim.head(10))
+
+import pandas as pd
+nadim=pd.read_csv('data.csv')
+print(nadim.tail(10))
+
+
+import pandas as pandas
+nadim=pd.read_csv('data.csv')
+print(nadim.info())
